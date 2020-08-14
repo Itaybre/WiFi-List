e3ec4c9df4cc85edcb29be3efe3f2a1220bc6137
Itay <itay@itaysoft.com>
2020-08-14 13:26:15 -0300

    Remove Code Align

--- MobileWiFi.h
+++ MobileWiFi.h
@@ -3,10 +3,11 @@
 typedef struct __WiFiNetwork* WiFiNetworkRef;
 typedef struct __WiFiManager* WiFiManagerRef;
 
-extern WiFiManagerRef   WiFiManagerClientCreate(CFAllocatorRef allocator, int flags);
-extern CFArrayRef       WiFiManagerClientCopyNetworks(WiFiManagerRef manager);
-extern CFStringRef      WiFiNetworkGetSSID(WiFiNetworkRef network);
-extern Boolean          WiFiNetworkIsWEP(WiFiNetworkRef network);
-extern Boolean          WiFiNetworkIsWPA(WiFiNetworkRef network);
-extern Boolean          WiFiNetworkIsEAP(WiFiNetworkRef network);
-extern CFStringRef      WiFiNetworkCopyPassword(WiFiNetworkRef network);
+extern WiFiManagerRef WiFiManagerClientCreate(CFAllocatorRef allocator, int flags);
+extern CFArrayRef WiFiManagerClientCopyNetworks(WiFiManagerRef manager);
+extern CFStringRef WiFiNetworkGetSSID(WiFiNetworkRef network);
+extern Boolean WiFiNetworkIsWEP(WiFiNetworkRef network);
+extern Boolean WiFiNetworkIsWPA(WiFiNetworkRef network);
+extern Boolean WiFiNetworkIsEAP(WiFiNetworkRef network);
+extern CFStringRef WiFiNetworkCopyPassword(WiFiNetworkRef network);
+extern CFDicrionaryRef WiFiNetworkCopyRecord(WiFiNetworkRef network)
